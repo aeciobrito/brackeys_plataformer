9876b43a8b2ba0ac2e7406a55af125a8b724450f
aeciobrito <aeciobrito@gmail.com>
2024-05-19 20:55:42 -0300

    Player 2.0

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,7 +4,7 @@ extends Area2D
 
 func _on_body_entered(body):
 	print("You Died!");
-	Engine.time_scale = 0.5;
+	Engine.time_scale = 0.3;
 	body.get_node("CollisionShape2D").queue_free();
 	timer.start();
 
