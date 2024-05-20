0605a79257595482150047b4733517a6cb5e9b60
aeciobrito <aeciobrito@gmail.com>
2024-05-20 17:07:24 -0300

    BugFix Coin

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -4,5 +4,5 @@ extends Area2D
 @onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body):
-	game_manager.add_point();
-	animation_player.play("pickup");
+	game_manager.add_point()
+	animation_player.play("pickup")
